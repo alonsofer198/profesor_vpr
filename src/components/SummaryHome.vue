16d1722ad2a78b18afa274a8ff7ce40a60be6e3a
alonso <caferp1204@gmail.com>
2022-10-05 13:57:28 -0500

    update

--- src/components/SummaryHome.vue
+++ src/components/SummaryHome.vue
@@ -8,9 +8,7 @@
 							<div class="col-12 col-md-8 text-center">
 								<h2 class="mt-3 titile-graph">Cantidad de profesores en UTUADO</h2>
 								<div class="mb-3 titile-graph2">Año Fiscal del 2009-2010 al 2021-2022</div>
-							</div>
-							
-							
+							</div>	
 						</div>						
 					</div>
 					<div class="col-12 col-md-12 position-relative">
